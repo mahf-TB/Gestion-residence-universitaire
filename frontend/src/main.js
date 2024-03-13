cb9b1e63db779b1a8d58b92494a9b5848d3ecf09
mahf-TB <mahefatsilavirintsoa@gmail.com>
2024-03-13 16:33:26 +0300

    add a package frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '../public/css/Tailwind.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'vue-multiselect/dist/vue-multiselect.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 createApp(App).use(store).use(router).mount('#app')
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h1 class="text-xl text-[#cc3737]">hello collaborateur</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
