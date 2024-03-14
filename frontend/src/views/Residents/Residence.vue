7f2ef18bf9b5f66e8e5025ead2c64a31521fc8b7
AnjahNyOny <anjahnyonyliantsoa@gmail.com>
2024-03-14 11:20:11 +0300

    ajouter lorem residences

--- frontend/src/views/Residents/Residence.vue
+++ frontend/src/views/Residents/Residence.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="">
       <h1 class="text-xxl text-[#2b6b88]">Actuallité Residence</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam dolores blanditiis odio quidem ipsum, atque inventore, rerum eos voluptatibus, sint dolore. Nisi repellendus temporibus velit dicta nulla! Hic, harum!
+      Ipsum blanditiis aperiam harum molestiae rem labore esse! Sit magni, perferendis modi pariatur corporis mollitia totam unde maiores eum molestias, ea rerum voluptas assumenda nostrum. Laborum dicta expedita vel provident?
+      Repellendus molestias omnis doloribus deleniti maxime sit laboriosam eaque consectetur iusto. Quidem reprehenderit nobis quibusdam. Soluta, perferendis est. Eveniet veniam earum deleniti laudantium esse labore odit ab voluptates expedita optio!
+      Ratione ducimus odit dignissimos velit perspiciatis totam, ipsum voluptates dolores doloribus corporis numquam repellendus voluptatem rerum pariatur quae eveniet facere, placeat sunt illum nostrum magni alias. Impedit ducimus doloribus officiis!
+      Aspernatur incidunt unde deleniti tempora rem, voluptatibus voluptate expedita aut maiores? Nihil molestiae, soluta unde enim odio, officiis deleniti corporis possimus sequi recusandae commodi. Ullam perferendis molestiae nesciunt maxime perspiciatis.
+      Aliquam voluptatem asperiores ratione atque quisquam ad. Amet voluptates beatae ea officiis magni asperiores ratione velit facilis iste tempore provident exercitationem eos rem eum quam numquam aut, deleniti quisquam consectetur.
+      Odit, aut quod libero eligendi iste obcaecati architecto ipsum neque harum tempora accusamus veritatis aperiam deleniti nostrum quos beatae at nam quibusdam. Excepturi, dolor quae officiis non sit et ab.
+      Deserunt vero ratione inventore quasi adipisci, corporis sapiente perspiciatis iure nostrum provident eligendi atque quia ad sunt saepe esse necessitatibus placeat ipsa animi recusandae suscipit! Magnam esse laborum alias impedit!
+      Enim, eveniet fugit expedita eum nisi nesciunt quia deleniti quam. Eos quas architecto commodi et exercitationem porro nemo. Incidunt laboriosam qui debitis excepturi blanditiis nam hic molestias pariatur perferendis repudiandae.
+      Doloremque facilis earum, rem obcaecati laboriosam quas dolores corporis dolor voluptate molestias! Repellat explicabo aliquam cupiditate, officiis quos itaque recusandae quam, eum modi consectetur hic deserunt unde laboriosam. Ratione, itaque.</p>
     </div>
   </template>
   
