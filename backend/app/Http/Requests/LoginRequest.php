a37e8cf5d3afa34668e426a18d112ba4f4a7f3cf
AnjahNyOny <anjahnyonyliantsoa@gmail.com>
2024-04-22 15:25:04 +0300

    vita merge ny activites

--- backend/app/Http/Requests/LoginRequest.php
+++ backend/app/Http/Requests/LoginRequest.php
@@ -22,7 +22,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [         
-            'email' => ['required', 'email:filter'],
+            'email' => ['required'],
             'password' => ['required', 'string'],
         ];
     }
