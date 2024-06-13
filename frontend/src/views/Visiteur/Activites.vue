97da4d0328b8a7635b552fd1a2979341cab7f853
AnjahNyOny <anjahnyonyliantsoa@gmail.com>
2024-06-13 10:42:05 +0300

    ajout activite de divertissement

--- frontend/src/views/Visiteur/Activites.vue
+++ frontend/src/views/Visiteur/Activites.vue
@@ -2,18 +2,20 @@
   <div>
     <sport v-if="getCas=='sport'"></sport>
     <education v-if="getCas=='education'"></education>
+    <divertissement v-if="getCas=='divertissement'"></divertissement>
   </div>
 </template>
 
 <script>
 import sport from "@/components/VisiteurComponents/activities/sport.vue";
 import education from "@/components/VisiteurComponents/activities/education.vue";
+import divertissement from '../../components/VisiteurComponents/activities/divertissement.vue';
 export default {
   name: "Activites",
-  components: {sport, education},
+  components: {sport, education, divertissement},
   data() {
     return {
-      cas:""
+
     }
   },
   computed : {
